644717ba3923f152df2b397c92e3687b32eee1b8
Siyah-Dev <dev.siyah@gmail.com>
2026-02-23 15:11:22 +0530

    card widget

--- test1/lib/features/home/presentation/screens/home_screen.dart
+++ test1/lib/features/home/presentation/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:test1/features/home/presentation/widgets/pickup_widget.dart';
 import 'package:test1/features/landing/presentation/controllers/landing_controller_provider.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
@@ -47,15 +48,18 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                     ],
                   ),
                   Container(
-                    
+                    padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(border: Border.all(), borderRadius: BorderRadius.all(Radius.circular(6))),
                     width: double.infinity,
                     child: Row(
+                      mainAxisAlignment: .spaceBetween,
                       children: [
-                        Column(children: [PickUpWidget(leading: 'A', subTitle: "Pickup",title: "150 Street New York, USA",), PickUpWidget(leading: 'B', subTitle: "Drop Off",title: "30 Street Boston, USA",)]),
+                        Column(spacing: 10,crossAxisAlignment: .start, children: [PickUpWidget(leading: 'A', subTitle: "Pickup",title: "150 Street New York, USA",), PickUpWidget(leading: 'B', subTitle: "Drop Off",title: "30 Street Boston, USA",)]),
                         Icon(Icons.swap_vert, color: Colors.grey),
                       ],
                     ),
                   ),
+                  Container(width: double.infinity,color: Colors.grey,)
                 ],
               ),
             ),
@@ -66,38 +70,3 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   }
 }
 
-class PickUpWidget extends StatelessWidget {
-  const PickUpWidget({
-    super.key,
-    required this.leading,
-    required this.subTitle,
-    required this.title,
-  });
-  final String leading;
-  final String subTitle;
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      child: Row(
-        children: [
-          CircleAvatar(
-            radius: 10,
-            backgroundColor: Colors.grey,
-            child: Text(leading, style: TextStyle(color: Colors.white)),
-          ),
-          Column(
-            children: [
-              Text(subTitle, style: TextStyle(color: Colors.grey)),
-              Text(
-                title,
-                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
---
+++ test1/lib/features/home/presentation/widgets/pickup_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class PickUpWidget extends StatelessWidget {
+  const PickUpWidget({
+    super.key,
+    required this.leading,
+    required this.subTitle,
+    required this.title,
+  });
+  final String leading;
+  final String subTitle;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Row(
+        crossAxisAlignment: .start,
+
+        children: [
+          CircleAvatar(
+            radius: 10,
+            backgroundColor: Colors.grey,
+            child: Text(leading, style: TextStyle(color: Colors.white)),
+          ),
+          SizedBox(width: 10,),
+          Column(
+            
+            crossAxisAlignment: .start,
+            children: [
+              Text(subTitle, style: TextStyle(color: Colors.grey)),
+              Text(
+                title,
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 12),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
